dc57743250289be8f1034678d1ee8098bd8bb519
NAM WOOK KIM <namwkim@seas.harvard.edu>
2015-05-15 19:20:57 -0400

    scripts

---
+++ budgeTag/scripts/extract-users.py
@@ -0,0 +1,20 @@
+
+import os, pymongo, sys, datetime, csv
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from collections import Counter
+
+if __name__ == "__main__":
+	# open remote database
+	
+
+	client = pymongo.MongoClient('54.191.187.64', 38716)
+	users = client.budgeTag.users.find()
+	
+	with open('users.csv', 'wb') as csvfile:
+		userCSV = csv.writer(csvfile)
+
+		for user in users:
+			for check in user['_checked']:
+				userCSV.writerow([user['username'], user['type'], check['issue'].encode("utf-8"), check['service'].encode("utf-8")])
